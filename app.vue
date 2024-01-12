8b2b32db7b665b8432c9c3791cf4ec1298f25f7d
al1mov <al1movdev.2008@gmail.com>
2024-01-12 22:48:59 +0500

    created meta tags

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
+<script setup>
+// import
+import Navbar from "@/components/Navbar.vue";
+
+useSeoMeta({
+  title: "Zamonaviy kinoteatr | Movitube",
+  ogTitle: "Zamonaviy kinoteatr | Movitube",
+  description: "Movitube yuqori sifatli, tezkor, zamonaviy kinoteatr!",
+  ogDescription: "Movitube yuqori sifatli, tezkor, zamonaviy kinoteatr!",
+  ogImage: "https://i.ibb.co/FJ22QK8/custom-fav.jpg",
+});
+</script>
 <template>
   <NuxtLoadingIndicator />
   <NuxtLayout>
+    <div class="navbar">
+      <Navbar />
+    </div>
     <NuxtPage />
   </NuxtLayout>
 </template>
