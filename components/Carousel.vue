90393e3dd8b3e244185f2da00494319a827b5434
al1mov <al1movdev.2008@gmail.com>
2024-01-12 22:49:42 +0500

    bug fixes at carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,13 +1,19 @@
+<script setup>
+import { Carousel, Slide } from "vue3-carousel";
+</script>
+
 <template>
-  <div class="w-[95%] m-auto container">
-    <Carousel :autoplay="2000" :wrap-around="true" class="bg-white">
+  <div class="w-[95%] m-auto container h-[500px]">
+    <Carousel
+      :autoplay="2000"
+      :wrap-around="true"
+      class="bg-white h-full rounded-2xl"
+    >
       <Slide v-for="slide in 10" :key="slide">
-        <div class="carousel__item">{{ slide }}</div>
+        <div class="w-full h-full">
+          <div class="text-content">{{ slide }}</div>
+        </div>
       </Slide>
     </Carousel>
   </div>
 </template>
-
-<script setup>
-import { Carousel, Slide } from "vue3-carousel";
-</script>
