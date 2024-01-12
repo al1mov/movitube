fbd8f2139c12aaec5b900d091d84755f146452d0
al1mov <al1movdev.2008@gmail.com>
2024-01-12 22:55:05 +0500

    maked mobile responsibility

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,15 @@
 <script setup>
 import Logo from "./ui-components/Logo.vue";
+import Menu from "./ui-components/Menu.vue";
+
+const isOpen = ref(false);
+
+const toggleHandler = () => {
+  isOpen.value = !isOpen.value;
+};
 </script>
 <template>
-  <div class="w-screen h-[60px] backdrop-blur-3xl bg-gray-500/30">
+  <div class="w-screen h-[60px] backdrop-blur bg-black/40 fixed z-50">
     <div
       class="w-[95%] h-full flex items-center justify-between m-auto container"
     >
@@ -13,16 +20,27 @@ import Logo from "./ui-components/Logo.vue";
         <div
           class="menu-items w-[200px] flex items-center justify-between ml-[50px]"
         >
-          <ul class="flex cursor-pointer justify-between w-full text-white">
+          <ul
+            class="hidden cursor-pointer justify-between w-full text-white sm:flex"
+          >
             <li>Bosh sahifa</li>
             <li>Kategoriyalar</li>
           </ul>
         </div>
+        <Menu v-if="isOpen" />
       </div>
-      <div
-        class="search-button w-[40px] h-[40px] flex justify-center items-center hover:bg-gray-500 transition duration-400 rounded-md cursor-pointer"
-      >
-        <Icon name="ic:baseline-search" class="text-2xl text-white" />
+      <div class="flex">
+        <div
+          @click="toggleHandler"
+          class="search-button w-[40px] h-[40px] flex justify-center items-center hover:bg-gray-500 transition duration-400 rounded-md cursor-pointer sm:hidden"
+        >
+          <Icon name="uil:align-center-alt" class="text-2xl text-white" />
+        </div>
+        <div
+          class="search-button w-[40px] h-[40px] flex justify-center items-center hover:bg-gray-500 transition duration-400 rounded-md cursor-pointer"
+        >
+          <Icon name="ic:baseline-search" class="text-2xl text-white" />
+        </div>
       </div>
     </div>
   </div>
